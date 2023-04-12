04e6a30f7a9009569e55083676aec4a3705c60cf
Navin Chandra <73417896+chandranavinn@users.noreply.github.com>
2023-04-12 14:53:34 +0530

    terraform expression on aws s3

---
+++ terraform exp 12001138/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# ConfigureAWS Provider
+provider "aws" {
+  region = var.region_name
+}
+
+variable "region_name" {
+  default     = "us-east-1"
+}
+
+variable "bucket_prefix" {
+  default     = "tfbucketnavin"
+
+}
+
+
+# CREATING BUCKET
+resource "aws_s3_bucket" "bucket" {
+  count = 2
+  bucket_prefix = "${count.index}-${var.bucket_prefix}"
+
+
+  tags = {
+    Name = "tfbucket"
+  }
+}
+
+
+output "bucket_id" {
+  value = resource.aws_s3_bucket.bucket
+}
